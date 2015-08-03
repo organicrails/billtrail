3716937e33402a45bf25302543b5d7b1a8dc2878
Jack <organicrails@gmail.com>
2015-08-03 12:02:18 -0700

    Add user-defined UUD to URL

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,9 +1,18 @@
 class Bill
   include Mongoid::Document
   field :event_name, type: String
+  field :urlID, type: String
 
-  # 1-N relationships for transactions
+  # creates a urlID that the users can refer to afterwards
+  field :_id, type: String, default: ->{ urlID }
+
+  # contains many transactions
   embeds_many :transactions
 
-  validates_presence_of :event_name
+  # validates presence of event_name and urlID.
+  validates_presence_of :event_name, :urlID
+
+  #validates the uniqueness of urlID
+  validates_uniqueness_of :urlID
+
 end
